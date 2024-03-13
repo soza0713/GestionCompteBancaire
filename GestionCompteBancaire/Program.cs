22033f65c127b89b819a0746e603f7ebdf73973d
Souleyman <aa@aaa.aaa>
2024-03-13 11:47:08 +0100

    Créer Compte terminé (Case1)

--- GestionCompteBancaire/Program.cs
+++ GestionCompteBancaire/Program.cs
@@ -4,13 +4,13 @@
     {
         static void Main(string[] args)
         {
-            //var account = new CompteBancaire("Diana", 1000);
-            //Console.WriteLine($"Le Compte {account.Numero} a été crée pour {account.Proprietaire} avec un solde de {account.Solde} Euros");
+            var account = new CompteBancaire("Diana", 1000);
+            Console.WriteLine($"Le Compte {account.Numero} a été crée pour {account.Proprietaire} avec un solde de {account.Solde} Euros");
 
-            //account.FaireRetrait(500, DateTime.Now, "Paiement du loyer");  // Ajoutez du code de test
-            //Console.WriteLine(account.Solde);
-            //account.FaireDepot(100, DateTime.Now, "Reboursement des copains");
-            //Console.WriteLine(account.Solde);
+            account.FaireRetrait(500, DateTime.Now, "Paiement du loyer");  // Ajoutez du code de test
+            Console.WriteLine(account.Solde);
+            account.FaireDepot(100, DateTime.Now, "Reboursement des copains");
+            Console.WriteLine(account.Solde);
             CompteBancaire compte = null;
             int choix = 1;
             do
@@ -21,7 +21,19 @@
 
                 {
                     case 1:
-
+                        account = CreerCompte();
+                        break;
+                    case 2:
+                        Console.WriteLine("Faire un depot");
+                        break;
+                    case 3:
+                        Console.WriteLine("Faire un retrait");
+                        break;
+                    case 4:
+                        Console.WriteLine("Afficher des Transaction");
+                        break;
+                    case 5:
+                        Console.WriteLine("Afficher le solde");
                         break;
                     case 0:
                         Console.WriteLine("Au revoir...");
@@ -40,6 +52,21 @@
             Console.WriteLine("Votre choix");
             var saisie = Console.ReadLine();
             return saisie;
+        }
+        private static CompteBancaire CreerCompte()
+        {
+            CompteBancaire compte = null;
+            string nom, solde;
+            decimal valeur = 0;
+            Console.WriteLine("Creer un compte");
+            Console.WriteLine("Quel votre nom?");
+            nom = Console.ReadLine();
+            Console.WriteLine("Quel est votre solde actuelle?");
+            solde = Console.ReadLine(); 
+            valeur = Convert.ToDecimal(solde);
+            compte = new CompteBancaire(nom, valeur);
+            Console.WriteLine("Votre compte bancaire viens d'être fait");
+            return compte;
         }
     }
 }
